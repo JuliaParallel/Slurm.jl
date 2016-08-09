2bd343130f589bebb14f14d5b3d25884afbceddf
Valentin Churavy <v.churavy@gmail.com>
2016-08-09 11:38:33 +0900

    Use dlopen with RTLD_GLOBAL

--- src/api.jl
+++ src/api.jl
@@ -1,6 +1,8 @@
 module api
 
 const libslurm = Libdl.find_library(["libslurm"])
+# We need to dlopen libslurm with RTLD_GLOBAL to make sure that all dependencies are loaded correctly.
+Libdl.dlopen(libslurm, Libdl.RTLD_GLOBAL)
 
 typealias pid_t UInt32
 typealias uid_t UInt32
